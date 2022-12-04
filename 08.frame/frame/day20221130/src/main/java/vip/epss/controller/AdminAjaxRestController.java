209c5c83f99b2975ac76099ae22a89c3d7abfab2
wangzhanf <wangzhanf@126.com>
2022-12-04 18:04:13 +0800

    20221204-2

--- 08.frame/frame/day20221130/src/main/java/vip/epss/controller/AdminAjaxRestController.java
+++ 08.frame/frame/day20221130/src/main/java/vip/epss/controller/AdminAjaxRestController.java
@@ -1,9 +1,7 @@
 package vip.epss.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import vip.epss.domain.Admin;
 
 @RequestMapping("/adminar")
 @RestController   //  @Controller 和  @ResponseBody 合体
@@ -11,6 +9,25 @@ public class AdminAjaxRestController {
 
     @GetMapping(value = "/opt/{aid}")
     public String get(@PathVariable("aid")Integer id){
-        return id.toString();
+        return "get:"+id.toString();
     }
+
+    @DeleteMapping(value = "/opt/{aid}")
+    public String delete(@PathVariable("aid")Integer id){
+        return "delete:"+id.toString();
+    }
+
+    @PostMapping(value = "/opt")
+    public String add(Admin admin){
+        System.out.println(admin);
+        return admin.toString();
+    }
+
+    @PutMapping(value = "/opt/{aid}")
+    public String update(@PathVariable("aid")Integer id,Admin admin){
+        System.out.println(admin);
+        return "id:" + id + "," + admin.toString();
+    }
+
+
 }
--- 08.frame/frame/day20221130/src/main/java/vip/epss/filter/CROSFilter.java
+++ 08.frame/frame/day20221130/src/main/java/vip/epss/filter/CROSFilter.java
@@ -26,6 +26,13 @@ public class CROSFilter implements Filter {
         httpServletResponse.setHeader("Access-Control-Allow-Origin",httpServletRequest.getHeader("Origin"));
         //设置开启Cookie
         httpServletResponse.setHeader("Access-Control-Allow-Credentials", "true");
+        //允许使用的HTTP方法 ， 不要和用POST伪装为PUT以及DELETE混淆
+        httpServletResponse.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE, PUT");
+        httpServletResponse.setHeader("Access-Control-Max-Age", "0");
+        httpServletResponse.setHeader("Access-Control-Allow-Headers", "Origin, No-Cache, X-Requested-With, If-Modified-Since, Pragma, Last-Modified, Cache-Control, Expires, Content-Type, X-E4M-With,userId,token");
+        httpServletResponse.setHeader("Access-Control-Allow-Credentials", "true");
+        httpServletResponse.setHeader("XDomainRequestAllowed","1");
+        httpServletResponse.setHeader("XDomainRequestAllowed","1");
         //返回到过滤器链，才可以继续处理
         filterChain.doFilter(httpServletRequest,httpServletResponse);
     }
